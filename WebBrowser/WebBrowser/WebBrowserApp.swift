76caff3290ddbf521867718cb67b6b553ce5acfd
Jun Hyeop <limjunehyeop@gmail.com>
2023-04-22 19:37:12 +0900

    first commit

---
+++ WebBrowser/WebBrowser/WebBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebBrowserApp.swift
+//  WebBrowser
+//
+//  Created by 임준협 on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WebBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
